16b89953bd480cdf655011aba8960c1fd9ca83ed
JarK <jarko@example.com>
2024-10-04 21:54:40 +0200

    added spring boot service

---
+++ springboot/src/test/java/com/github/jarko/playground/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.jarko.playground.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
