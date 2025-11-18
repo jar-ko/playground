ae24c27921bacae1e8bc8c4b0883600c96c0a4c0
JarKo <jarko@example.com>
2025-11-18 20:28:24 +0100

    initial commit

---
+++ src/main/java/com/github/jarko/playground/PlaygroundApplication.java
@@ -0,0 +1,12 @@
+package com.github.jarko.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(PlaygroundApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/jarko/playground/PlaygroundApplicationTest.java
@@ -0,0 +1,14 @@
+package com.github.jarko.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class PlaygroundApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
